4321653b4cd3a679510fa68cf780b2ced6899167
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-09-11 13:31:04 -0500

    moved nav to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,14 +10,20 @@
     if (res.ok) {
       const data = await res.json();
       console.log('__layout.svelte load: data =', data);
-      //return {context: data, maxage: 10};
-      return {context: data};
+      return {context: data, maxage: 10};
     }
 
     const error = await res.text();
     return {error, status: res.status};
   }
 </script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/page/1">Page 1</a>
+  <a href="/page/2">Page 2</a>
+  <a href="/page/3">Page 3</a>
+</nav>
 
 <h1>My Layout</h1>
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
-<nav>
+<!--nav>
   <a href="/page/1">Page 1</a>
   <a href="/page/2">Page 2</a>
   <a href="/page/3">Page 3</a>
-</nav>
+</nav-->
+
+<h1>Home</h1>
