93df4a4645b9024046f77f4a027ecd7462afb425
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-09-12 19:32:51 -0500

    testing use of adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
+import netlify from '@sveltejs/adapter-netlify';
+//import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: netlify()
+    //adapter: vercel()
+  }
 };
 
 export default config;
